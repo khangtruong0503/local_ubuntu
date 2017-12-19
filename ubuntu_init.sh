04620745997f59364528f7dcd363efb32653958f
Unknown <khangzhang0503@gmail.com>
2017-12-19 22:33:39 +0700

    fix chome, vs code

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-# this script is using for my ubuntu mate
+# this script is using for my ubuntu
 
 # update system
-sudo apt update && sudo apt upgrade -y
+sudo apt update -y
 
 # install zsh: shell theme
 sudo apt-get install zsh -y
 
 # make zsh is default shell
 sudo chsh -s $(which zsh)
+
+# install tweak tool
+sudo apt-get install unity-tweak-tool
+
+# install unikey
+sudo apt-get install ibus-unikey
+ibus restart
 
 # install curl
 sudo apt-get install curl -y
@@ -19,15 +26,26 @@ sudo apt-get install git -y
 # install nodejs v8
 sudo curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install nodejs -y
- 
-# install google chrome
-sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome*.deb
 
 # install bower: frontend libaries manager
-sudo npm install -g bower
+sudo npm install bower -g
 
-#install code editor: visual studio code
+# install gulp
+sudo npm install gulp-cli -g
+ 
+# install google chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get update
+sudo apt-get install google-chrome-stable -y
+
+# install code editor: visual studio code
 sudo curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get update
+sudo apt-get install code
+
+# install git kraken
+wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
+sudo dpkg -i gitkraken-amd64.deb
