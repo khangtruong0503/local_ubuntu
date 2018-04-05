dbe076448c177c28b540fb7b944bda68557aabf4
Unknown <khangzhang0503@gmail.com>
2018-04-05 20:26:05 +0700

    remove themes, icons, add java, jenkins, angular cli

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -36,12 +36,13 @@ sudo npm install bower -g
 
 # install gulp
 sudo npm install gulp-cli -g
+
+# install angular cli
+sudo npm install @angular/cli -g
  
 # install google chrome
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-sudo apt-get update
-sudo apt-get install google-chrome-stable -y
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb -y
 
 # install code editor: visual studio code
 sudo curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
@@ -54,22 +55,22 @@ sudo apt-get install code
 wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
 sudo dpkg -i gitkraken-amd64.deb
 
-# install theme: Arc Theme Dark
-sudo add-apt-repository ppa:noobslab/themes -y
+# install java
 sudo apt-get update
-sudo apt-get install arc-theme -y
+sudo apt-get install default-jre -y
+sudo apt-get install default-jdk -y
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
 
-# install theme: Adapta Dark
-sudo apt-add-repository ppa:tista/adapta -y
+# install jenkins
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
-sudo apt-get install adapta-gtk-theme -y
+sudo apt-get install jenkins -y
 
-# install icon: Paper
-sudo add-apt-repository ppa:snwh/pulp -y
-sudo apt-get update
-sudo apt-get install paper-icon-theme -y
 
-# install icon: Vivacious Colors
-sudo add-apt-repository ppa:ravefinity-project/ppa -y
-sudo apt-get update
-sudo apt-get install vivacious-colors -y
+
+
+
+
